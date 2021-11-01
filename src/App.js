e5521c9582f2708c72e4203535f1ec09ad88e579
unknown <sorv19@hotmail.comgit config --global user.name Santiago!>
2021-11-01 13:53:21 -0300

    barra menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import NavBar from './Components/navbar/navbar';
+import Menu from './Components/menu';
+
 
 function App() {
   return (
     <div className="App">
         <NavBar/>
+        <Menu/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Menu from './Components/menu';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+);
+
+
+ReactDOM.render(
+  <StyledEngineProvider injectFirst>
+    <Menu />
+  </StyledEngineProvider>,
+  document.querySelector("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
